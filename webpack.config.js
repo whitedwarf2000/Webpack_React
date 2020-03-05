a85e1bc5ea8801703cb4ab01ae222112db9ae5b2
Son Le <sontdt69@gmail.com>
2020-03-05 16:58:07 +0700

    Initial base React project with Webpack

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: __dirname + "/src/index.js",
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + "/src/public/index.html",
+      inject: 'body'
+    }),
+
+  ],
+  devServer: {
+    contentBase: './src/public',
+    port: 3000,
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), path.resolve(__dirname, 'node_modules')],
+    extensions: ['.js']
+  },
+  devtool: 'eval-source-map'
+};
